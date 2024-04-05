7bc2d9edc7b7920c2882a19bd2830d4cc803e1cf
Ahmed Yesuf <anurye.yesuf@gmail.com>
2024-04-05 09:26:04 +0200

    modified performPickAndPlace.m

--- performPickAndPlace.m
+++ performPickAndPlace.m
@@ -1,11 +1,14 @@
 % performPickAndPlace.m: sample script to perform a simple pick and place
 % operation.
 
-% specify the pick and place positions
+%% Initialize libraries
+initialize
+
+%% specify the pick and place positions
 pickPose = [19, 15, 2];
 placePose = [19.5 -15 2];
 
-% perform the simulation
+%% perform the simulation
 simulationPickAndPlace(pickPose, placePose);
 
 % perform pick and place hardware
